26f6177ee31604728c295c6744f073193fb68f99
vigor-marketing <vigoroiltools@gmail.com>
2026-08-07 15:40:00 +0800

    Add workbench BFF and todo API

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 4173 },
+  server: {
+    port: 4173,
+    proxy: {
+      '/api': 'http://127.0.0.1:4000',
+    },
+  },
 })
